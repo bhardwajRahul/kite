24575414123d8447f2e6a6c745d1f010b9e60bfe
Zzde <zhangxh1997@gmail.com>
2026-04-27 11:48:31 +0800

    fix: set analytics default false in db, ref: https://github.com/kite-org/kite/commit/fe799c1e4c5fea11624c91ad845cd73ac93b1d31
    Signed-off-by: Zzde <zhangxh1997@gmail.com>

--- pkg/model/general_setting.go
+++ pkg/model/general_setting.go
@@ -39,7 +39,7 @@ type GeneralSetting struct {
 	KubectlEnabled          bool         `json:"kubectlEnabled" gorm:"column:kubectl_enabled;type:boolean;not null;default:true"`
 	KubectlImage            string       `json:"kubectlImage" gorm:"column:kubectl_image;type:varchar(255);not null;default:'zzde/kubectl:latest'"`
 	NodeTerminalImage       string       `json:"nodeTerminalImage" gorm:"column:node_terminal_image;type:varchar(255);not null;default:'busybox:latest'"`
-	EnableAnalytics         bool         `json:"enableAnalytics" gorm:"column:enable_analytics;type:boolean;not null;default:true"`
+	EnableAnalytics         bool         `json:"enableAnalytics" gorm:"column:enable_analytics;type:boolean;not null;default:false"`
 	EnableVersionCheck      bool         `json:"enableVersionCheck" gorm:"column:enable_version_check;type:boolean;not null;default:true"`
 	PasswordLoginDisabled   bool         `json:"passwordLoginDisabled" gorm:"column:password_login_disabled;type:boolean;not null;default:false"`
 	JWTSecret               SecretString `json:"-" gorm:"column:jwt_secret;type:text"`
